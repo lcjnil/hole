08349df494c551af298bc1c6fbc5c9290c55f2f0
NiL <emlcjnil@gmail.com>
2014-06-25 20:09:27 +0800

    as

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var testRoutes = require('./routes/test')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/test', testRoutes);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
